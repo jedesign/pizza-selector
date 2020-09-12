7ce827fc931edebf9b548dd130aa43961c17d7e7
Jonas Thomi <jonas.thomi@outlook.com>
2020-09-12 18:49:59 +0200

    Nieder mit der Oberschicht!

--- src/js/components/Pizza.vue
+++ src/js/components/Pizza.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="bg-blue-900 h-40 flex items-center justify-center text-center font-extrabold text-xl" >{{ pizza.name }}
+  <div class="bg-blue-900 h-40 flex items-center justify-center text-center font-extrabold text-xl relative" style="background-image: url(https://source.unsplash.com/MQUqbmszGGM/500x180)">
+  <div class="flex justify-center items-center absolute top-0 left-0 h-full w-full bg-blue-900 bg-opacity-75">{{ pizza.name }}</div>
   </div>
 </template>
 
