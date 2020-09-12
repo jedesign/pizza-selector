2ca81bbeaf20dc7c67c5eb6a5098995f9c4796c1
Marco Rieser <marco.rieser@jedesign.ch>
2020-09-12 19:02:35 +0200

    change pizza bg color

--- src/js/components/Pizza.vue
+++ src/js/components/Pizza.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="bg-blue-900 h-40 flex items-center justify-center text-center font-extrabold text-xl relative" style="background-image: url(https://source.unsplash.com/MQUqbmszGGM/500x180)">
-  <div class="flex justify-center items-center absolute top-0 left-0 h-full w-full bg-blue-900 bg-opacity-75">{{ pizza.name }}</div>
-  </div>
+  <div class="bg-gray-800 h-40 flex items-center justify-center text-center font-extrabold text-xl">{{ pizza.name }}</div>
 </template>
 
 <script>
