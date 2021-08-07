6f565eab1721a5ee74e3fcecf9a74547fd2c9ccc
Marco Rieser <marco.rieser@jedesign.ch>
2021-08-07 21:16:43 +0200

    add dynamic title for ingredients selection

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -5,7 +5,7 @@
 
       <div class="w-full">
         <div class="flex justify-start items-center">
-          <h2 class="text-4xl mb-2 mt-2 p-1 mr-10">Deine Zutaten</h2>
+          <h2 class="text-4xl mb-2 mt-2 p-1 mr-10">{{ingredientsTitle}}</h2>
           <div
               class="bg-gray-800 font-semibold rounded-lg hover:bg-pink-500 button flex justify-center items-center cursor-pointer"
               v-show="selectedIngredients.length > 0"
@@ -53,6 +53,17 @@ export default {
     };
   },
   computed: {
+    ingredientsTitle() {
+      const titles = [
+          'Deine Zutaten',
+          'I wött:',
+          'Gib:',
+          'Habe Bock auf:',
+          'Uf wa häsch Lust?',
+          'Da sind dini Optione:'
+      ];
+      return titles[Math.floor(Math.random() * titles.length)];
+    },
     filteredPizzas() {
       let pizzas = [];
       if (this.selectedIngredients.length < 1) {
