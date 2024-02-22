30f00bae3b953213085daafe49e59b7195a90398
Marco Rieser <marco@rieser.dev>
2024-02-22 09:00:35 +0100

    update token and isolate node version

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export default {
 	airTableApp: 'appVmn1bbhGZnSf04',
-	apiToken: 'keyVYwB7qkm7tus1Z',
+	apiToken: 'patO75YQSPTKt5XxM.13101d722cab93f23745129709c9f51df41452fd463ce14c49b6faf8bd5c5ddf',
 };
